03ce34da7f27f570ae391f56034d2ae76b82506f
Mateusz Miszczak <mthw.miszczak@gmail.com>
2023-03-29 17:26:46 +0200

    Initial js file with data that I will need for the development

---
+++ script.js
@@ -0,0 +1,40 @@
+// DATA FOR THE APPLICATION
+const account1 = {
+  owner: 'Mateusz Miszczak',
+  movements: [100, 200, -140, 4000, -1200, -60, 80, 2400],
+  interestRate: 1.3, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Andrew Huberman',
+  movements: [6000, 4300, 20000, -9000, -2500, -2000, 10000, -15],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Jordan Peterson',
+  movements: [2000, -100, 40, -600, -10, 5000, 4000, -600],
+  interestRate: 1.5,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Howard Hamlin',
+  movements: [30000, 10000, 8000, 5000, -9000],
+  interestRate: 1.5,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// ELEMENTS
+
+// Labels
+
+// Containers
+
+// Buttons
+
+// Inputs
